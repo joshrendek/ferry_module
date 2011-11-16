3f91c110930ba53ce190193df0f8c967b3eea46b
Greg Lombardo <shadowssong@gmail.com>
2011-11-15 21:54:29 -0500

    more housekeeping

--- ferry_module.c
+++ ferry_module.c
@@ -7,18 +7,11 @@
 
 MODULE_LICENSE("GPL");
 
-/* Global Vars */
-struct bank {
-    int queen;
-    int nobles;
-    int peasants;
-};
-
 /* Function prototypes */
 void init_vars(void);
 void ferry_loop(void);
-void print_curr(void);
 void begin_ferry(void);
+void release_passengers(void);
 
 /* Extern system call stub declarations */
 extern long (*STUB_ferry_start)(void);
@@ -27,17 +20,22 @@ extern long (*STUB_ferry_stop)(void);
 
 /* PROC declarations */
 static struct proc_dir_entry *proc_entry;
-static const char *PROC_NAME = "ferry2";
+static const char *PROC_NAME = "ferry";
 int proc_read(char *page, char **pages, off_t offset, int page_size, int *eof, void *data);
 
-
+/* Global Vars */
+struct bank {
+	int queen;
+	int nobles;
+	int peasants;
+};
 
 struct ferry {
-    int queen;
-    int nobles;
-    int peasants;
-    int compartments;
-    int curr_bank;	//0 = north, 1 = south, 2 = in transit
+	int queen;
+	int nobles;
+	int peasants;
+	int compartments;
+	int curr_bank;	//0 = north, 1 = south, 2 = in transit
 };
 
 struct bank north_bank;
@@ -51,77 +49,64 @@ struct mutex lock;
 
 long my_ferry_start()
 {
-    printk("%s: Ferry started!\n", __FUNCTION__);
-    mutex_init(&lock);
-    init_vars();	//set all of the variables in the structs to 0
-    
-    //Initialize proc entry
-    proc_entry = create_proc_entry(PROC_NAME, 0444, NULL);
-    
-    //begin ferry thread
-    ferry_thread = kthread_run(begin_ferry, NULL, "ferry_thread");
-    
-    return 0;
+	printk("%s: Ferry started!\n", __FUNCTION__);
+	//initialize mutex
+	mutex_init(&lock);		
+	init_vars();		//set all of the variables in the structs to 0
+
+	//Initialize proc entry
+	proc_entry = create_proc_entry(PROC_NAME, 0444, NULL);
+
+	//begin ferry thread
+	ferry_thread = kthread_run(begin_ferry, NULL, "ferry_thread");
+
+	return 0;
 }
 
 void begin_ferry()
 {
-    int i;
-    set_current_state(TASK_RUNNING);
-    
-    while(stop_ferry_flag != 1)
-    	ferry_loop();
-    
-    /*for(i = 15; i > 0; i--)
-    {
-        if(stop_ferry_flag == 1)
-            break;
-        else
-            ferry_loop();
-    }*/
-    
-    printk("%s: **THREAD STOPPING**\n", __FUNCTION__);
+	//Set thread state to running
+	set_current_state(TASK_RUNNING);
+
+	//Continue looping ferry until stopped
+	while(stop_ferry_flag != 1)
+		ferry_loop();
+
+	printk("%s: **THREAD STOPPING**\n", __FUNCTION__);
 }
 
-void print_curr()
+
+int proc_read(char *page, char **pages, off_t offset, int page_size, int *eof, void *data)
 {
-    printk("%s: North: Queen: %d\n", __FUNCTION__, north_bank.queen);
-    printk("%s: North: Nobles: %d\n", __FUNCTION__, north_bank.nobles);
-    printk("%s: North: Peasants: %d\n", __FUNCTION__, north_bank.peasants);
-    printk("%s: South: Queen: %d\n", __FUNCTION__, south_bank.queen);
-    printk("%s: South: Nobles: %d\n", __FUNCTION__, south_bank.nobles);
-    printk("%s: South: Peasants: %d\n", __FUNCTION__, south_bank.peasants);
-}
+	//Writing to proc file
+	int ret, pass_aboard;
+	char * bank;
+	if(curr_ferry.curr_bank == 0)
+		bank = "North";
+	else if(curr_ferry.curr_bank == 1)
+		bank = "South";
+	else if(curr_ferry.curr_bank == 2)
+		bank = "In Transit";
+	else
+		bank = "Unknown";
 
-int proc_read(char *page, char **pages, off_t offset, int page_size, int *eof, void *data){
-    int ret, pass_aboard;
-    char * bank;
-    if(curr_ferry.curr_bank == 0)
-        bank = "North";
-    else if(curr_ferry.curr_bank == 1)
-        bank = "South";
-    else if(curr_ferry.curr_bank == 2)
-        bank = "In Transit";
-    else
-        bank = "Unknown";
+	pass_aboard = curr_ferry.queen + curr_ferry.nobles + curr_ferry.peasants;
     
-    pass_aboard = curr_ferry.queen + curr_ferry.nobles + curr_ferry.peasants;
-    
-    ret = sprintf(page, 
+	ret = sprintf(page, 
                   "Current bank: %s\nPassengers aboard: %d\nCurrent passengers:\nQueen: %d\nNobles: %d\nPeasants: %d\nNorth bank:\nQueen: %d\nNobles: %d\nPeasants: %d\nSouth bank:\nQueen: %d\nNobles: %d\nPeasants: %d\nTotal ferried: %d\n", 
                   bank, pass_aboard, curr_ferry.queen, curr_ferry.nobles, curr_ferry.peasants, 
                   north_bank.queen, north_bank.nobles, north_bank.peasants, south_bank.queen, 
                   south_bank.nobles, south_bank.peasants, total_ferried);
-    return ret;
+    	return ret;
 }
 
 void release_passengers()
 {
-    curr_ferry.queen = 0;
-    curr_ferry.nobles = 0;
-    curr_ferry.peasants = 0;
-    curr_ferry.compartments = 0;
-    curr_ferry_passengers = 0;
+	curr_ferry.queen = 0;
+	curr_ferry.nobles = 0;
+	curr_ferry.peasants = 0;
+	curr_ferry.compartments = 0;
+	curr_ferry_passengers = 0;
 }
 
 void ferry_loop()
@@ -135,6 +120,7 @@ void ferry_loop()
 
 	//lock
 	mutex_lock_interruptible(&lock);
+	printk("%s: **Locked**\n", __FUNCTION__);
 
 	release_passengers();
 
@@ -175,11 +161,10 @@ void ferry_loop()
 
 	//releaseLock()
 	mutex_unlock(&lock);
+	printk("%s: **Unlocked**\n", __FUNCTION__);
 
-	printk("%s: **IN TRANSIT**\n", __FUNCTION__);
 	set_current_state(TASK_INTERRUPTIBLE);
-
-	//schedule();
+	//schedule(); why is this needed?
 	msleep(2000);
 	set_current_state(TASK_RUNNING);
 	curr_ferry.curr_bank = 2; //in transit
@@ -190,7 +175,7 @@ void ferry_loop()
 
 	//lock
 	mutex_lock_interruptible(&lock);
-
+	printk("%s: **Locked**\n", __FUNCTION__);
 	release_passengers();
 
 	//load passengers
@@ -230,8 +215,8 @@ void ferry_loop()
 
 	//release lock
 	mutex_unlock(&lock);
+	printk("%s: **Unlocked**\n", __FUNCTION__);
 
-	printk("%s: **IN TRANSIT**\n", __FUNCTION__);
 	set_current_state(TASK_INTERRUPTIBLE);
 	//schedule();
 	msleep(2000);
@@ -240,20 +225,20 @@ void ferry_loop()
 
 void init_vars()
 {
-    north_bank.queen = 0;
-    north_bank.nobles = 0;
-    north_bank.peasants = 0;
-    south_bank.queen = 0;
-    south_bank.nobles = 0;
-    south_bank.peasants = 0;
-    curr_ferry.queen = 0;
-    curr_ferry.nobles = 0;
-    curr_ferry.peasants = 0;
-    curr_ferry.curr_bank = 0;	//starts at north
-    curr_ferry.compartments = 0;
-    curr_ferry_passengers = 0;
-    total_ferried = 0;
-    stop_ferry_flag = 0;		//0 = keep going, 1 = stop
+	north_bank.queen = 0;
+	north_bank.nobles = 0;
+	north_bank.peasants = 0;
+	south_bank.queen = 0;
+	south_bank.nobles = 0;
+	south_bank.peasants = 0;
+	curr_ferry.queen = 0;
+	curr_ferry.nobles = 0;
+	curr_ferry.peasants = 0;
+	curr_ferry.curr_bank = 0;	//starts at north
+	curr_ferry.compartments = 0;
+	curr_ferry_passengers = 0;
+	total_ferried = 0;
+	stop_ferry_flag = 0;	//0 = keep going, 1 = stop
 }
 
 long my_ferry_request(int passenger_type, char start_bank)
@@ -319,31 +304,28 @@ long my_ferry_request(int passenger_type, char start_bank)
 
 long my_ferry_stop(void)
 {
-    long ret = 0;
-    printk("%s: Ferry stop \n", __FUNCTION__);
-    if(stop_ferry_flag == 1)
-        ret = 1;	//Already stopping
-    else
-        stop_ferry_flag = 1;	//ALL STOP!
-    
-    //We need to unload all passengers first
-    //Maybe click a flag that will break on each ferry loop
-    
-    remove_proc_entry(PROC_NAME, 0);
-    return ret;
+	long ret = 0;
+	printk("%s: Ferry stop \n", __FUNCTION__);
+	if(stop_ferry_flag == 1)
+		ret = 1;	//Already stopping
+	else
+		stop_ferry_flag = 1;	//ALL STOP!
+
+	remove_proc_entry(PROC_NAME, 0);
+	return ret;
 }
 
 my_module_init() {
-    STUB_ferry_start=&(my_ferry_start);
-    STUB_ferry_request=&(my_ferry_request);
-    STUB_ferry_stop=&(my_ferry_stop);
-    return 0;
+	STUB_ferry_start=&(my_ferry_start);
+	STUB_ferry_request=&(my_ferry_request);
+	STUB_ferry_stop=&(my_ferry_stop);
+	return 0;
 }
 
 my_module_exit() {
-    STUB_ferry_start=NULL;
-    STUB_ferry_request=NULL;
-    STUB_ferry_stop=NULL;
+	STUB_ferry_start=NULL;
+	STUB_ferry_request=NULL;
+	STUB_ferry_stop=NULL;
 }
 
 module_init(my_module_init);
--- usp_sample.c
+++ usp_sample.c
@@ -3,49 +3,51 @@
 #include <sys/syscall.h>
 #include <linux/unistd.h>
 
+//define syscall numbers
 #define __NR_ferry_start 345
 #define __NR_ferry_request 346
 #define __NR_ferry_stop 347
 
 int main()
 {
-	//testing start
-	int test1=25;
-	long ret1;
-	ret1=syscall(__NR_ferry_start);
+	//Start ferry
+	long ret1 = syscall(__NR_ferry_start);
 	if(ret1!=0)
 		perror("Ferry_start error");
 	else
 		printf("Ferry thread started\n");
 
 	//testing request
-	int request1=2;
-	char request2='n';
-	long ret2;
-	ret2=syscall(__NR_ferry_request, request1, request2);
-	if(ret2==1)
+	long ret2 = syscall(__NR_ferry_request, 2, 'n');
+	if(ret2 == 1)
 		perror("Issue_Request Error");
 	else
 		printf("Request successful\n");
 
-	long ret3 = syscall(__NR_ferry_request, 2, 'n');
-	long ret4 = syscall(__NR_ferry_request, 0, 'n');
-	long ret5 = syscall(__NR_ferry_request, 1, 'n');
-	long ret6 = syscall(__NR_ferry_request, 1, 'n');
-	long ret7 = syscall(__NR_ferry_request, 2, 'n');
-	long ret8 = syscall(__NR_ferry_request, 2, 'n');
-	long ret9 = syscall(__NR_ferry_request, 2, 'n');
-	long ret10 = syscall(__NR_ferry_request, 1, 's');
-	long ret11 = syscall(__NR_ferry_request, 0, 's');
-	long ret12 = syscall(__NR_ferry_request, 1, 's');
-	long ret13 = syscall(__NR_ferry_request, 2, 's');
-	long ret14 = syscall(__NR_ferry_request, 1, 's');
-	long ret15 = syscall(__NR_ferry_request, 2, 's');
-	long ret16 = syscall(__NR_ferry_request, 0, 's');
-	long ret17 = syscall(__NR_ferry_request, 2, 's');
-	long ret18 = syscall(__NR_ferry_request, 1, 's');
-	
+	//begin random requests with random wait times
 
+	syscall(__NR_ferry_request, 2, 'n');
+	sleep(3);
+	syscall(__NR_ferry_request, 0, 'n');
+	sleep(2);
+	syscall(__NR_ferry_request, 1, 'n');
+	syscall(__NR_ferry_request, 1, 'n');
+	sleep(1);
+	syscall(__NR_ferry_request, 2, 'n');
+	syscall(__NR_ferry_request, 2, 'n');
+	syscall(__NR_ferry_request, 2, 'n');
+	syscall(__NR_ferry_request, 1, 's');
+	sleep(4);
+	syscall(__NR_ferry_request, 0, 's');
+	syscall(__NR_ferry_request, 1, 's');
+	syscall(__NR_ferry_request, 2, 's');
+	syscall(__NR_ferry_request, 1, 's');
+	sleep(1);
+	syscall(__NR_ferry_request, 2, 's');
+	syscall(__NR_ferry_request, 0, 's');
+	sleep(5);
+	syscall(__NR_ferry_request, 2, 's');
+	syscall(__NR_ferry_request, 1, 's');
 	sleep(2);
 	syscall(__NR_ferry_request, 1, 's');
 	sleep(3);
@@ -78,18 +80,12 @@ int main()
 	sleep(6);
 	syscall(__NR_ferry_request, 1, 'n');
 
+	//Stopping ferry
 	long stop_ret = syscall(__NR_ferry_stop);
 	if(stop_ret==1)
 		perror("Ferry already stopping");
 	else
 		printf("Ferry stopped successfully\n");
-	//testing stop
-	/*int test3=45;
-	long ret3;
-	ret3=syscall(__NR_ferry_stop, test3);
-	if(ret3<0)
-		perror("system call error");
-	else
-		printf("Function successful, returned %i\n", ret3);*/
+
 	return 0;
 }
